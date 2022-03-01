10f51358dde08b4103f117cb3f4d42d83d90e0fa
rajmahal4 <rsingh6@mail.sfsu.edu>
2022-03-01 13:51:36 -0800

    fixed sorting

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ I thought this was interesting nonetheless. Overall I feel like I learned more a
 
 I work full time and learn code in my spare time, I do not think its good for me to switch between node/express and ruby. I want to really focus on one language at a time and become comfortable. 
 
-I cannot complete this assignment. I made it to step 1, I coudlnt implement the sortBy function or the direction function. 
+I could not do the testing. 
+
+Overall this was a cool project. In the future I will give ruby on rails the time it deserves and make some cool apps with it. 
+
+Instructions:
+bundle install
+
+rails db:migrate
+
+rails s 
+
+heres an example url 
+
+http://localhost:3000/posts?tag=tech&sortBy=likes&direction=desc
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,34 @@ require 'json'
 class PostsController < ApplicationController
   def index
     if(post_params_sort)
-      uri = URI('https://api.hatchways.io/assessment/blog/posts?tag=' + post_params )
-      res = Net::HTTP.get_response(uri)
-      data = JSON.parse(res.body)
-      data.each { |d| puts json_response(d[1][0]["likes"]) }
+      if(post_params_sort == 'id' ||post_params_sort == 'reads' ||post_params_sort == 'likes'|| post_params_sort == 'popularity' )
+        uri = URI('https://api.hatchways.io/assessment/blog/posts?tag=' + post_params )
+        res = Net::HTTP.get_response(uri)
+        data = JSON.parse(res.body)
+
+        if(post_params_direction == 'desc')
+          arr = sort_result_desc(data)
+        else
+          arr = sort_result(data)
+        end
+        json_response(arr)
+      else
+        render json: {error:  "sortBy parameter is invalid"}
+      end
     else
       uri = URI('https://api.hatchways.io/assessment/blog/posts?tag=' + post_params )
       res = Net::HTTP.get_response(uri)
       json_response(res.body)
     end
+  end
+
+  def sort_result(array)
+    arr = array['posts'].sort_by {|_key, value| _key['likes']}
+    return arr
+  end
+  def sort_result_desc(array)
+    arr = array['posts'].sort_by {|_key, value| _key['likes']}.reverse
+    return arr
   end
 
  private
@@ -23,6 +42,11 @@ class PostsController < ApplicationController
   def post_params_sort
     params[:sortBy]
   end
+  def post_params_direction
+    params[:direction]
+  end 
+
+
 
   
 end
